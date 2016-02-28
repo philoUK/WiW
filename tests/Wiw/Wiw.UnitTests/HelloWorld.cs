d6d706d3506ac2e2a44f5b6d4bd1ab9b94e4cbd0
philoUK <pbennett@neworbit.co.uk>
2016-02-28 15:45:49 +0000

    initial project creation

---
+++ tests/Wiw/Wiw.UnitTests/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿namespace Wiw.UnitTests
+{
+    using Xunit;
+
+    public class HelloWorld
+    {
+        [Fact]
+        public void Setup_Ignore()
+        {
+            Assert.True(true);
+        }
+    }
+}
